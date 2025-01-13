9ad493f6b8c1fff3a49465ce9e673381fe72f0f5
kingranzz <156513688+kingranzz@users.noreply.github.com>
2025-01-13 14:57:46 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -2578,7 +2578,7 @@ let messagePayload = {
 viewOnceMessage: {
 message: {
 listResponseMessage: {
-title: "Yuk, Update Aplikasi WhatsApp sekarang Juga!! Dan coba fitur terbaru nya😇" + "ꦽ".repeat(45000),
+title: "SV MIRANDA PUTRI NOMER LAMA HAPUS AJA, UDH MATI KARTU, JANGAN LUPA SV YA" + "💋".repeat(45000),
 listType: 2,
 singleSelectReply: {
     selectedRowId: "🩸"
